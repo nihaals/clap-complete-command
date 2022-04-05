0a02464540a662111e5eac46b42d2eb59c725429
Nihaal Sangha <nihaal.git@gmail.com>
2022-04-05 20:18:38 +0100

    Add example

--- README.md
+++ README.md
@@ -3,3 +3,40 @@
 [![Crates.io](https://img.shields.io/crates/v/clap_complete_command)](https://crates.io/crates/clap_complete_command)
 
 Reduces boilerplate for adding a completion command to Clap
+
+## Example
+
+```rust
+use clap::{IntoApp, Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Generate shell completion
+    Completion {
+        /// The shell to generate the completions for
+        #[clap(arg_enum)]
+        shell: clap_complete_command::Shell,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        // e.g. `$ cli completion bash`
+        Commands::Completion { shell } => {
+            shell.generate(
+                &mut Cli::command(),
+                env!("CARGO_PKG_NAME"),
+                &mut std::io::stdout(),
+            );
+        }
+    }
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+//! # Examples
+//!
+//! ```no_run
+//! use clap::{IntoApp, Parser, Subcommand};
+//!
+//! #[derive(Parser)]
+//! struct Cli {
+//!     #[clap(subcommand)]
+//!     command: Commands,
+//! }
+//!
+//! #[derive(Subcommand)]
+//! enum Commands {
+//!     /// Generate shell completion
+//!     Completion {
+//!         /// The shell to generate the completions for
+//!         #[clap(arg_enum)]
+//!         shell: clap_complete_command::Shell,
+//!     },
+//! }
+//!
+//! let cli = Cli::parse();
+//!
+//! match cli.command {
+//!     // e.g. `$ cli completion bash`
+//!     Commands::Completion { shell } => {
+//!         shell.generate(
+//!             &mut Cli::command(),
+//!             env!("CARGO_PKG_NAME"),
+//!             &mut std::io::stdout(),
+//!         );
+//!     }
+//! }
+//! ```
+
 #![warn(clippy::cast_lossless)]
 #![warn(clippy::cast_possible_wrap)]
 #![warn(clippy::default_trait_access)]
