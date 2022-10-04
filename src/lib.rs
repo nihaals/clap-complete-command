06a7f1d6c85f263244b3608ec14900e5dfe9d315
Nihaal Sangha <nihaal.git@gmail.com>
2022-10-04 14:20:03 +0100

    Fix formatting of docstring

--- src/lib.rs
+++ src/lib.rs
@@ -38,20 +38,20 @@
 //!
 //! fn build_cli() -> Command {
 //!     Command::new(env!("CARGO_PKG_NAME"))
-//!     .subcommand_required(true)
-//!     .subcommand(
-//!         Command::new("completions")
-//!             .about("Generate shell completions")
-//!             .arg(
-//!                 Arg::new("shell")
-//!                     .value_name("SHELL")
-//!                     .help("The shell to generate the completions for")
-//!                     .required(true)
-//!                     .value_parser(
-//!                         clap::builder::EnumValueParser::<clap_complete_command::Shell>::new(),
-//!                     ),
-//!             ),
-//!     )
+//!         .subcommand_required(true)
+//!         .subcommand(
+//!             Command::new("completions")
+//!                 .about("Generate shell completions")
+//!                 .arg(
+//!                     Arg::new("shell")
+//!                         .value_name("SHELL")
+//!                         .help("The shell to generate the completions for")
+//!                         .required(true)
+//!                         .value_parser(
+//!                             clap::builder::EnumValueParser::<clap_complete_command::Shell>::new(),
+//!                         ),
+//!                 ),
+//!         )
 //! }
 //!
 //! let matches = build_cli().get_matches();
